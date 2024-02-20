a4320ee6505d40728de018ef695dc74e3c84f8f7
domna <florian.dobener@physik.hu-berlin.de>
2024-02-20 16:14:16 +0100

    Adds github workflows

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+
+
+def remove(filepath):
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+    elif os.path.isdir(filepath):
+        shutil.rmtree(filepath)
+
+if not {{cookiecutter.github_workflows}}:
+    remove('.github')
