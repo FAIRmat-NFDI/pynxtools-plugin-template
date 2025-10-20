bdb97dd55de65e24938787f8491509a09546c908
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2025-10-20 17:43:13 +0200

    add script to generate test .nxs files

---
+++ pynxtools-{{cookiecutter.reader_name}}/src/pynxtools_{{cookiecutter.reader_name}}/scripts/generate_reference_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+READER={{cookiecutter.reader_name}}
+NXDL={{cookiecutter.class_name}}
+
+project_dir=$(dirname $(dirname $(realpath $0)))
+cd $project_dir/tests/data
+
+function update_ref_file {
+  local FOLDER=$1
+  local NXDL=$2
+  echo "Update $FOLDER reference file for $NXDL"
+  files=$(find . -type f \( ! -name "*.log" -a ! -name "*.nxs" \))
+  dataconverter ${files[@]} --reader $READER --nxdl $NXDL --ignore-undocumented --output "$FOLDER_ $NXDL_ref.nxs" &> ref_output.txt
+  cd ..
+}
+
+project_dir=$(dirname $(dirname $(realpath $0)))
+
+folders=(
+  ""
+)
+
+nxdls={{cookiecutter.supported_nxdls}}
+
+for folder in "${folders[@]}"; do
+  for nxdl in "${nxdls[@]}"; do
+    cd $project_dir/tests/data
+    update_ref_file "$folder" "$nxdl"
+  done
+done
