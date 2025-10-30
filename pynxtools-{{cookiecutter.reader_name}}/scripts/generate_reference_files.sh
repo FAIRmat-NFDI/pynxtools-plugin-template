d58390f62af15ff2439b49743d346d316cd71597
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2025-10-30 13:21:17 +0100

    update script for reference generation

--- pynxtools-{{cookiecutter.reader_name}}/scripts/generate_reference_files.sh
+++ pynxtools-{{cookiecutter.reader_name}}/scripts/generate_reference_files.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
-READER={{cookiecutter.reader_name}}
-NXDL={{cookiecutter.supported_nxdl}}
-
-project_dir=$(dirname $(dirname $(realpath $0)))
-cd $project_dir/tests/data
-
 function update_ref_file {
   local FOLDER=$1
   local NXDL=$2
+  cd $FOLDER
   echo "Update $FOLDER reference file for $NXDL"
   files=$(find . -type f \( ! -name "*.log" -a ! -name "*.nxs" \))
-  dataconverter ${files[@]} --reader $READER --nxdl $NXDL --ignore-undocumented --output "$FOLDER_ $NXDL_ref.nxs" &> ref_output.txt
+  dataconverter ${files[@]} --reader $READER --nxdl $NXDL --ignore-undocumented --output "${FOLDER}_ref.nxs" # &> ref_output.txt
   cd ..
 }
-
-project_dir=$(dirname $(dirname $(realpath $0)))
 
 folders=(
   ""
 )
 
-nxdls={{cookiecutter.supported_nxdl}}
+READER="{{cookiecutter.reader_name}}"
+nxdls=(
+  "{{cookiecutter.supported_nxdl}}"
+)
+
+project_dir=$(dirname $(dirname $(realpath $0)))
+cd $project_dir/tests/data
+
 
 for folder in "${folders[@]}"; do
   for nxdl in "${nxdls[@]}"; do
-    cd $project_dir/tests/data
     update_ref_file "$folder" "$nxdl"
   done
 done
