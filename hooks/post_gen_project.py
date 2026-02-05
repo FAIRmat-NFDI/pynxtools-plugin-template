cd2519c0a8a5c9457df9c59ba08d029ef0307df2
lukaspie <lukaspie@github.com>
2026-02-05 12:36:57 +0100

    small fixes for post gen hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,10 +3,9 @@ from typing import Optional
 import stat
 from pathlib import Path
 import shutil
-import subprocess
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("post_gen_project")
+logger: logging.Logger = logging.getLogger("post_gen_project")
 
 ALL_TEMP_FOLDERS = ["licenses", "py_sources"]
 PY_SOURCES = Path("py_sources")
@@ -81,7 +80,7 @@ if __name__ == "__main__":
         if condition != "False"
     ]
     if variants:
-        module_name = f"pynxtools_{{cookiecutter.reader_name}}"
+        module_name = "pynxtools_{{cookiecutter.reader_name}}"
         src_nomad = root / "src" / module_name / "nomad"
         tests = root / "tests" / "nomad"
 
@@ -104,7 +103,6 @@ if __name__ == "__main__":
             for test_file in (PY_SOURCES / "tests" / "nomad").iterdir():
                 if test_file.is_file() and any(variant in test_file.name for variant in variants):
                     dst_file = tests / test_file.name
-                    print(test_file, dst_file)
                     shutil.copy(test_file, dst_file)
                     logger.info("Copied test %s → %s", test_file, dst_file)
 
